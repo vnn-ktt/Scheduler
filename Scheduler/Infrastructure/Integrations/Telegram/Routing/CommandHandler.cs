22439d56b6f488953f622044754f51a7cf966359
Platunov Ivan <platunov@seaproject.ru>
2026-09-04 12:06:44 +0300

    create profile

--- Scheduler/Infrastructure/Integrations/Telegram/Routing/CommandHandler.cs
+++ Scheduler/Infrastructure/Integrations/Telegram/Routing/CommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Scheduler.Infrastructure.Integrations.Telegram.Screens;
+using Scheduler.Infrastructure.Integrations.Telegram.State;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 
@@ -6,11 +7,13 @@ namespace Scheduler.Infrastructure.Integrations.Telegram.Routing;
 
 public sealed class CommandHandler
 {
+    private readonly TelegramSessionStore _sessionStore;
     private readonly MainMenuHandler _mainMenuHandler;
 
-    public CommandHandler(MainMenuHandler mainMenuHandler)
+    public CommandHandler(MainMenuHandler mainMenuHandler, TelegramSessionStore sessionStore)
     {
         _mainMenuHandler = mainMenuHandler;
+        _sessionStore = sessionStore;
     }
 
     public async Task HandleAsync(
@@ -18,8 +21,27 @@ public sealed class CommandHandler
         Message message,
         CancellationToken cancellationToken)
     {
-        if (message.Text is null)
+        if (message.Text is null ||
+            message.From is null)
+        {
             return;
+        }
+
+        var state = _sessionStore.Get(
+            message.From.Id
+        );
+
+        if (state != TelegramConversationState.None)
+        {
+            await HandleConversationStateAsync(
+                bot,
+                message,
+                state,
+                cancellationToken
+            );
+
+            return;
+        }
 
         switch (message.Text)
         {
@@ -30,6 +52,66 @@ public sealed class CommandHandler
                     cancellationToken
                 );
                 break;
+
+            default:
+                await bot.SendMessage(
+                    chatId: message.Chat.Id,
+                    text: "Используйте /start",
+                    cancellationToken: cancellationToken
+                );
+                break;
         }
     }
+
+    private async Task HandleConversationStateAsync(
+        ITelegramBotClient bot,
+        Message message,
+        TelegramConversationState state,
+        CancellationToken cancellationToken)
+    {
+        switch (state)
+        {
+            case TelegramConversationState.WaitingForProviderName:
+                await HandleProviderNameAsync(
+                    bot,
+                    message,
+                    cancellationToken
+                );
+                break;
+        }
+    }
+    private async Task HandleProviderNameAsync(
+    ITelegramBotClient bot,
+    Message message,
+    CancellationToken cancellationToken)
+    {
+        var name = message.Text?.Trim();
+
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            await bot.SendMessage(
+                chatId: message.Chat.Id,
+                text: "Название не может быть пустым.",
+                cancellationToken: cancellationToken
+            );
+
+            return;
+        }
+
+        _sessionStore.Clear(
+            message.From!.Id
+        );
+
+        await bot.SendMessage(
+            chatId: message.Chat.Id,
+            text:
+            $"""
+            Профиль создан.
+
+            Имя: {name}
+            """,
+                cancellationToken: cancellationToken
+            );
+    }
+
 }
